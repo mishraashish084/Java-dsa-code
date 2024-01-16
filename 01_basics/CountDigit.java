d7fe27744816fc69f9cda526676d77331e6bb38c
mishraashish084 <69692489+mishraashish084@users.noreply.github.com>
2024-01-16 15:52:21 +0000

    Count Digit using Method

---
+++ 01_basics/CountDigit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class CountDigit {
+    static int Digit(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+        System.out.println("Number of digits: " + Digit(n));
+    }
+}

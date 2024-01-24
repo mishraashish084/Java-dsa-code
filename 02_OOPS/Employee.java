74f084c78f629500b99f19db686a636e3b5c79c9
mishraashish084 <69692489+mishraashish084@users.noreply.github.com>
2024-01-24 14:41:01 +0000

    Getter and setter

---
+++ 02_OOPS/Employee.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Employee{
+   private String name;
+    private int rno;
+    private int per;
+public String getName()
+{
+    return name;
+}
+public void setName(String name)
+{
+    this.name=name;
+}
+public int getRoll()
+{
+    return rno;
+}
+public void  setRoll(int rno)
+{
+    this.rno=rno;
+}
+public int getPer()
+{
+    return per;
+}
+public void setPer(int per)
+{
+    this.per=per;
+}
+    
+}
---
+++ 02_OOPS/EmployeeTest.java
@@ -0,0 +1,13 @@
+
+public class EmployeeTest{
+    public static void main(String args[])
+    {
+        Employee s1=new Employee();
+        s1.setName("Ashish");
+        s1.setRoll(1);
+        s1.setPer(89);
+        System.out.println(s1.getName());
+        System.out.println(s1.getRoll());
+        System.out.println(s1.getPer());
+    }
+}
